6f23edd3fe849b9262cce7e44c55f3d115a28177
ailenpaez <a.jorgelinapaez@gmail.com>
2023-11-03 21:28:20 -0300

    Añadiendo variables de entorno en el proyecto

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 import net from "node:net"
+import dotenv from "dotenv"
+dotenv.config()
 const serverTCP = net.createServer()
+
+const PORT = process.env.PORT; // 65656
 
 serverTCP.on("connection", ()=>{
     console.log("Client Connected :)");
 })
 
-serverTCP.listen (1234, () =>{
+serverTCP.listen (PORT, () =>{
     console.log("server UP");
 })
